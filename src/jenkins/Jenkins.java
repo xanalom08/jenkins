006b022e04fe68c142b7cdf33a76548fdd2c736f
Xavi Navarro <xanalom08@gmail.com>
2017-05-22 12:32:19 -0400

    IC Test using Jenkins

---
+++ src/jenkins/Jenkins.java
@@ -0,0 +1,29 @@
+
+package jenkins;
+
+/**
+ *
+ * @author Xavi
+ */
+import java.io.*;
+public class Jenkins {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        File f = new File("index.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");bw.newLine();
+            bw.write("  <head>");bw.newLine();
+            bw.write("    <title>");bw.newLine();
+            bw.write("      Working with Jenkins CI");bw.newLine();
+            bw.write("    </title>");bw.newLine();
+            bw.write("  </head>");bw.newLine();
+            bw.write("  <body>");bw.newLine();
+            bw.write("    CI Jenkins test");bw.newLine();
+            bw.write("  </body>");bw.newLine();
+            bw.write("</html>");bw.newLine();
+            bw.close();
+        }
+    }
+}
